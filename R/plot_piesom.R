5a43f7eb91faf34f28eb8af0ac6adeb7b13a6234
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2020-11-14 21:38:45 +0900

    hoge

--- R/plot_piesom.R
+++ R/plot_piesom.R
@@ -132,11 +132,13 @@ plot.piesom.property <- function(object){
 #' @importFrom kohonen unit.distances
 #' @importFrom kohonen object.distances
 #' @importFrom ggrepel geom_text_repel
+#' @importFrom ggforce geom_circle
+#' @importFrom ggforce geom_rect
 #' @return
 #' @export
 #'
 #' @examples
-plot.piesom.map <- function(object, labels = NULL){
+plot.piesom.map <- function(object, labels = NULL, grid.labels = NULL){
   response <- model.response(model.frame(object$formula, data = object$data))
   a <- tibble(groups = response, id = object$som$unit.classif)
   if(!is.null(labels)){
@@ -150,16 +152,121 @@ plot.piesom.map <- function(object, labels = NULL){
   cddist <- as.matrix(object.distances(object$som, type = "codes"))
   cddist[abs(ndist - 1) > .001] <- NA
   neigh.dists <- colMeans(cddist, na.rm = TRUE)
-  data_ <- mutate(b, groups = NA, distance = neigh.dists, id = NA)
+  
   gg <- ggplot(data)
-  gg <- gg + geom_circle(aes(r = 0.5, x0 = x, y0 = y, alpha = distance), fill = "grey",data = data_)
+  clust <- NA
+  if(!is.null(grid.labels)){
+    clust <- grid.labels
+  }
+  data_ <- mutate(b, groups = clust, distance = neigh.dists, id = NA)
+  if(!is.null(grid.labels)){
+    gg <- gg + geom_rect(aes(xmin = x - 0.5, xmax = x + 0.5, ymin = y - 0.5, ymax = y + 0.5, fill = groups), data = data_)
+  }
+  gg <- gg + geom_circle(aes(r = 0.5, x0 = x, y0 = y, alpha = distance), fill = "grey", data = data_)
   if(is.null(labels)){
     gg <- gg + geom_jitter(aes(x = x, y = y, col = groups), alpha = 1,size = 3, width = 0.2, height = 0.2)
   }else{
     gg <- gg + geom_text_repel(aes(x = x, y = y, col = groups, label = labels), alpha = 1)
   }
-  gg <- gg + scale_fill_gradient(low = "grey95", high = "grey70")
   gg <- gg + ggtitle("SOM")
   gg
+}
+
+
+hclust.piesom <- function(obj, data_label = NULL){
+  codes <- obj$som$codes[[1]]
+  id_node_with_data <- sort(unique(obj$som$unit.classif))
+  codes <- codes[id_node_with_data,]
+  grid <- with(obj$som$grid, glue("[SOM{1:nrow(pts)}({pts[,1]}, {pts[,2]})]"))
+  label <- NULL;
+  if(!is.null(data_label)){
+    label <- tibble(label = data_label, classif = obj$som$unit.classif) %>% 
+      group_by(classif) %>% 
+      summarise(label = paste(label, collapse = " "), .groups = "drop") %>% 
+      arrange(classif) %>% 
+      pull(label)
+    assert_that(are_equal(nrow(codes), length(label)))
+  }
+  if (is.null(label)) {
+    label <- grid[id_node_with_data]
+  }else{
+    label <- paste(label, grid[id_node_with_data])
+  }
+  rownames(codes) <- label
+  clust <- hclust(dist(codes))
+  attr(clust$labels, "grid_id") <- id_node_with_data
+  clust
+}
+
+
+#' Title
+#'
+#' @param object 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+piesom_data <- function(object){
+  res <- list()
+  res$somgrid <- data.frame(object$som$grid$pts)
+  res$somgrid.circle <- rename(res$somgrid, x0 = x, y0 = y, r = 1)
+  res$somgrid.rect <- transmute(res$somgrid, 
+                                xmin = x - 0.5, 
+                                xmax = x + 0.5,
+                                ymin = y - 0.5, 
+                                ymax = y + 0.5)
+  res$classif <- left_join(mutate(res$somgrid, id = seq_along(x)), data.frame(id = object$som$unit.classif), by = c(id = "id"))
+  res$pc.score <- data.frame(
+    x = object$pc$scores[,1],
+    y = object$pc$scores[,2]
+  )
+  res$pc.score.som.pre <- data.frame(
+    x = object$init.score[,1], 
+    y = object$init.score[,2], 
+    x.somgrid = object$grid$pts[,1], 
+    y.somgrid = object$grid$pts[,2])
+  
+  somgrid2pc.score.som.pre.x <- with(res$pc.score.som.pre, set_names(x, paste(x.somgrid, y.somgrid)))
+  somgrid2pc.score.som.pre.y <- with(res$pc.score.som.pre, set_names(y, paste(x.somgrid, y.somgrid)))
+  res$segmentsbyx.som.pc.score.pre <- data.frame(
+    x_ = object$som$grid$pts[, 1],
+    xend_ = object$som$grid$pts[, 1],
+    y_ = object$som$grid$pts[, 2],
+    yend_ = object$som$grid$pts[, 2] + 1,
+    x.somgrid = object$som$grid$pts[, 1]
+  ) %>% 
+    filter(yend_ <= max(y_)) %>% 
+    transmute(
+      x = somgrid2pc.score.som.pre.x[paste(x_, y_)],
+      xend = somgrid2pc.score.som.pre.x[paste(xend_, yend_)],
+      y = somgrid2pc.score.som.pre.y[paste(x_, y_)],
+      yend = somgrid2pc.score.som.pre.y[paste(xend_, yend_)],
+      x.somgrid = x.somgrid
+    )
+  res$pc.score.som.post <- data.frame(
+    x = object$som.score[,1], 
+    y = object$som.score[,2], 
+    x.somgrid = object$grid$pts[,1], 
+    y.somgrid = object$grid$pts[,2])
+  
+  somgrid2pc.score.som.post.x <- with(res$pc.score.som.post, set_names(x, paste(x.somgrid, y.somgrid)))
+  somgrid2pc.score.som.post.y <- with(res$pc.score.som.post, set_names(y, paste(x.somgrid, y.somgrid)))
+  res$segmentsbyx.som.pc.score.post <- data.frame(
+    x_ = object$som$grid$pts[, 1],
+    xend_ = object$som$grid$pts[, 1],
+    y_ = object$som$grid$pts[, 2],
+    yend_ = object$som$grid$pts[, 2] + 1,
+    x.somgrid = object$som$grid$pts[, 1]
+  ) %>% 
+    filter(yend_ <= max(y_)) %>% 
+    transmute(
+      x = somgrid2pc.score.som.post.x[paste(x_, y_)],
+      xend = somgrid2pc.score.som.post.x[paste(xend_, yend_)],
+      y = somgrid2pc.score.som.post.y[paste(x_, y_)],
+      yend = somgrid2pc.score.som.post.y[paste(xend_, yend_)],
+      x.somgrid = x.somgrid
+    )
+  return(res)
 }
 
